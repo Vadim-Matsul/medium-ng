5c03c375d1c6b811e8ad68c58c1314ec299fccf7
Vadim Matsul <vmatsul@devvela.com>
2023-04-17 12:35:46 +0300

    MA-12: add banner shared module

--- src/app/globalFeed/globalFeed.module.ts
+++ src/app/globalFeed/globalFeed.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { BannerModule } from '../shared/modules/banner/banner.module';
 import { FeedModule } from '../shared/modules/feed/feed.module';
 import { GlobalFeedRoutingModule } from './globalFeed-routing.module';
 import { GlobalFeedComponent } from './components/globalFeed/globalFeed.component';
@@ -12,6 +13,7 @@ import { GlobalFeedComponent } from './components/globalFeed/globalFeed.componen
     /** project */
     GlobalFeedRoutingModule,
     FeedModule,
+    BannerModule,
   ],
   declarations: [GlobalFeedComponent],
 })
